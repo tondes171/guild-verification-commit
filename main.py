1172b8dc9ec429e9ec43f40089000c81f87c563b
tondes171 <32002020+tondes171@users.noreply.github.com>
2025-11-01 21:29:35 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-01T18:28:44.403Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-01T18:29:34.594Z
+Commit: Resolve null pointer exception in user service
